36708e84d5bf7ed895018ee95ecb7a21d106dd3b
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2020-09-30 18:41:15 +0200

    Updates

---
+++ precache-manifest.09a99248adc22deb85d81c38de2ddfaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d877bc4f568cfe30cf3629b55d14a5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e7f38f5b181d0f3b36",
+    "url": "/static/css/main.eb85bcf0.chunk.css"
+  },
+  {
+    "revision": "dd65e85536be92cde285",
+    "url": "/static/js/2.0336b21c.chunk.js"
+  },
+  {
+    "revision": "4ba9857954d77a8ae0400c451d8716ce",
+    "url": "/static/js/2.0336b21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e7f38f5b181d0f3b36",
+    "url": "/static/js/main.94054b75.chunk.js"
+  },
+  {
+    "revision": "b4ab9d216b14b8d559fb",
+    "url": "/static/js/runtime-main.1654b609.js"
+  }
+]);

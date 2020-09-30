5b73a74e5bba28ef09f54b0c73a3f706e7940b88
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2020-09-30 18:50:54 +0200

    Updates

---
+++ precache-manifest.226b589f74e6d6df27b9452a6fc0c276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bdd7a2b4f9a6a3d55afab963019b5e",
+    "url": "/uniswapdelegates/index.html"
+  },
+  {
+    "revision": "cbd8b09ad2cf4bf0ebc8",
+    "url": "/uniswapdelegates/static/css/main.eb85bcf0.chunk.css"
+  },
+  {
+    "revision": "460faa313b46973b8d5a",
+    "url": "/uniswapdelegates/static/js/2.3b011594.chunk.js"
+  },
+  {
+    "revision": "4ba9857954d77a8ae0400c451d8716ce",
+    "url": "/uniswapdelegates/static/js/2.3b011594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd8b09ad2cf4bf0ebc8",
+    "url": "/uniswapdelegates/static/js/main.b365567b.chunk.js"
+  },
+  {
+    "revision": "f216b792440344676d6e",
+    "url": "/uniswapdelegates/static/js/runtime-main.391b5796.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09a99248adc22deb85d81c38de2ddfaa.js"
+  "/uniswapdelegates/precache-manifest.226b589f74e6d6df27b9452a6fc0c276.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uniswapdelegates/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

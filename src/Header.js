d9a2a69de7835354167da7ebe388d21d2b9a7082
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2020-10-15 18:02:18 +0200

    Added some addresses. Pwettier header.

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return <div>
-    <h1 style={{ margin: 0 }}>
-      <Link to={'/'} className="anonlink"><img alt="Uniswap governance" src={require('./unicorn.png')} style={{display: 'inline', height: 30}} />Uniswap governance</Link>
+    <h1 className="headertitle">
+      <Link to={'/'} className="anonlink"><img alt="Uniswap delegates" src={require('./unicorn.png')} className="headeravatar" />Uniswap delegates</Link>
     </h1>
   </div>
 }
